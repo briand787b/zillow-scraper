922b5fcd64e33351ec21aebf914537d5a6053cf5
brian dostilio <briand787b@gmail.com>
2020-05-20 19:38:58 -0400

    react video @33:34

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Modified!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div id="page-body">
+          <Route path="/">
+            <HomePage />
+          </Route>
+          <Route>
+            
+          </Route>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
