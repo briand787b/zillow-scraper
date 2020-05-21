0021b6c2ad1ff1a5e6387e636482bacec4432fda
brian dostilio <briand787b@gmail.com>
2020-05-21 18:37:52 -0400

    video @68

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
 import ArticleListPage from './pages/ArticlesListPage';
 import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 import NavBar from './components/NavBar';
 
@@ -16,6 +17,7 @@ function App() {
       <div className="App">
         <NavBar />
         <div id="page-body">
+          <Switch>
           <Route path="/" exact>
             <HomePage />
           </Route>
@@ -28,6 +30,10 @@ function App() {
           <Route path="/article/:name">
             <ArticlePage />
           </Route>
+          <Route>
+            <NotFoundPage/>
+          </Route>
+          </Switch>
         </div>
       </div>
     </BrowserRouter>
--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
+import NotFoundPage from './NotFoundPage'
 
 const ArticlePage = () => {
     const { name } = useParams();
 
     const article = articles.find(article => article.name == name );
 
-    return (
+    return article 
+        ? (
         <>
         <h1>{article.title}</h1>
         {article.content.map((paragraph, id) => (
             <p key={id}>{paragraph}</p>
         ))}
         </>
+    ) : (
+        <NotFoundPage/>
     );
 }
 
