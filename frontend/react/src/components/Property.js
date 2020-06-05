e94d94a43f35c912dd7ccb06ab7082c79cbca34d
brian <bdostilio@sparkfly.com>
2020-06-05 18:54:18 -0400

    properties list page is complete

--- frontend/react/src/components/Property.js
+++ frontend/react/src/components/Property.js
@@ -6,10 +6,8 @@ import '../styles/property.css';
 
 const Property = ({ url, imageURL, acreage, address, currentPrice, recentChange }) => (
         <div className="property">
-            <h3 className="address">{address}</h3>
             <img className="property-image" src={imageURL} alt={address}/>
-            <PropertyFeatures {...{acreage, currentPrice, recentChange}}/>
-            <a className="url" href={url}>Visit Property</a>
+            <PropertyFeatures {...{acreage, address, currentPrice, recentChange, url}}/>
         </div>
 )
 
--- frontend/react/src/components/PropertyFeatures.js
+++ frontend/react/src/components/PropertyFeatures.js
@@ -2,17 +2,26 @@ import React from 'react';
 
 import '../styles/propertyFeatures.css';
 
-const PropertyFeatures = ({ url, acreage, currentPrice, recentChange }) => (
+const PropertyFeatures = ({ acreage, address, currentPrice, recentChange, url }) => (
     <div className="features">
         <div className="cost">
             <span className="cost-currency">$</span>
             <span className="cost-dollars">
-                {currentPrice / 1000 }K
+                {currentPrice / 1000}K
             </span>
         </div>
+        <h3 className="address">{address}</h3>
         <div className="cta-button" ></div>
         <div className="specs">
-            <span className="acreage">Acreage: {acreage}ac</span>
+            <span className="acreage">{acreage} Acres | ${currentPrice / acreage}/Acre</span>
+            <iframe
+                    width="300"
+                    height="300"
+                    frameborder="0"
+                    src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyB76MwBO4v3QDDYLy6o4r7DpOBESqFcZ7A&q=${encodeURIComponent(address)}`}
+                    allowfullscreen
+            ></iframe>
+            <a className="url" href={url}>Visit Property</a>
         </div>
     </div >
 );
